870a3763fbe9ad2217e778244dc0f5eecfee4571
fkxxyz <fkxxyz@163.com>
2023-04-14 16:52:23 +0800

    fix: rev chatgpt web engine set timeout

--- engine/rev_chatgpt_web.py
+++ engine/rev_chatgpt_web.py
@@ -51,30 +51,30 @@ class RevChatGPTWeb:
 
     def accounts(self, level: int = 65536) -> requests.Response:
         params = {'level': level}
-        return self.__session.get(self.__url + '/api/account/valid', params=params)
+        return self.__session.get(self.__url + '/api/account/valid', params=params, timeout=60)
 
     def set_title(self, account: str, id_: str, title: str) -> requests.Response:
         params = {'account': account, 'id': id_}
         data = title.encode('utf-8')
-        return self.__session.patch(self.__url + '/api/title', params=params, data=data)
+        return self.__session.patch(self.__url + '/api/title', params=params, data=data, timeout=60)
 
     def history(self, account: str, id_: str) -> requests.Response:
         params = {'account': account, 'id': id_}
-        return self.__session.get(self.__url + '/api/history', params=params)
+        return self.__session.get(self.__url + '/api/history', params=params, timeout=60)
 
     def send(self, account: str, msg: str, id_: str = '', mid: str = '') -> requests.Response:
         params = {'account': account, 'id': id_, 'mid': mid}
-        return self.__session.post(self.__url + '/api/send', params=params, data=msg.encode('utf-8'))
+        return self.__session.post(self.__url + '/api/send', params=params, data=msg.encode('utf-8'), timeout=60)
 
     def get(self, mid: str, stop=False) -> requests.Response:
         params = {'mid': mid}
         method = "PATCH" if stop else "GET"
-        return self.__session.request(method, self.__url + '/api/get', params=params)
+        return self.__session.request(method, self.__url + '/api/get', params=params, timeout=60)
 
     def delete(self, account: str, id_: str) -> requests.Response:
         params = {'account': account, 'id': id_}
-        return self.__session.delete(self.__url + '/api/conversation', params=params)
+        return self.__session.delete(self.__url + '/api/conversation', params=params, timeout=60)
 
     def counter(self, account: str, n: int = 1) -> requests.Response:
         params = {'account': account, 'n': n}
-        return self.__session.patch(self.__url + '/api/account/lock', params=params)
+        return self.__session.patch(self.__url + '/api/account/lock', params=params, timeout=60)
