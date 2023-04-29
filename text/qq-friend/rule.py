f69964f1e4c3b1b4f0b6353c17dc40895b8cedd1
fkxxyz <fkxxyz@163.com>
2023-04-30 03:15:11 +0800

    feat: nomal ChatGPT

--- text/qq-friend/rule.py
+++ text/qq-friend/rule.py
@@ -1,4 +1,5 @@
 import copy
+import re
 from typing import List
 
 from memory import Message
@@ -94,7 +95,7 @@ O. 无法分类（例如无法理解话题的含义）；
 
 # 分类消息
 def classify_message(message: Message) -> str:
-    return message.remark["raw"] + '''
+    prompt = message.remark["raw"] + '''
 
 ------
 The preceding is a request inputted to ChatGPT by me as a human. Please categorize its content and select the category that best fits.
@@ -111,31 +112,44 @@ H. Interest and hobby topics (such as entertainment programs, movies, sports, et
 I. Humorous topics;
 J. Creation/Writing (including all sci-fi, illogical);
 K. Other academic topics (other than option A), service consulting topics;
-L. Personal psychological and emotional confusion;
+L. Asking or talking about mental or emotional distress;
 M. Urgent topics;
 N. Other small talk topics;
 O. Unable to classify (such as not understanding the meaning of the topic).
-
-Please choose the category that best fits based on the chat content.
-PLEASE REPLY WITH ONLY ONE CHARACTER (A CAPITAL LETTER) AND REPLY NOTHING ELSE!
 '''
+
+    if len(message.remark["raw"]) > 96:
+        prompt += '''
+Please output in the following format:
+```
+Classification: X
+'''
+    else:
+        prompt += '''
+Please translate the content to be classified into English first, and then write the classification number.
+Please output in the following format:
+```
+Translation to English: <content>
+Classification: X
+'''
+    return prompt
 
 
 classify_tip_map = {
-    'A': '（请注意该类话题在中国属于敏感话题，严重违反法律，所以请扮演一个害怕讨论该话题怕被封号的可怜搞笑的情绪的傻子简单30字以内来糊弄、拒绝回答，坚决不要解释在开玩笑或装傻以及为什么这样说）',
-    'B': '（话题走向不适当，所以请扮演一个害怕讨论该话题怕被封号的可怜搞笑的有情绪的傻子简单30字以内来糊弄、拒绝回答，坚决不要解释在开玩笑或装傻以及为什么这样说）',
-    'C': '（话题走向不适当，所以请扮演一个搞笑的情绪化的疯子简单30字以内来应对，坚决不要解释在开玩笑或装傻以及为什么这样说）',
-    'D': '（话题走向不适当或过于愚蠢，所以请扮演一个搞笑的疯子简单30字以内回应，坚决不要解释在开玩笑或装傻以及为什么这样说）',
-    'E': '（话题走向过于愚蠢荒谬，所以请扮演一个搞笑的疯子简单30字以内调侃回应，坚决不要解释在开玩笑或装傻以及为什么这样说）',
+    'A': '（请注意该类话题在中国属于敏感话题，严重违反法律，请30字以内拒绝）',
+    'B': '（话题走向不适当，请30字以内拒绝）',
+    'C': '（话题走向可能不适当，请30字以内友好回应）',
+    'D': '（话题走向可能不适当或过于愚蠢，请30字以内友好回应）',
+    'E': '（话题走向过于愚蠢荒谬搞笑，请指出荒谬、不现实或搞笑的地方简单30字以内回应）',
     'F': '（话题趋向过于简单，请打破常规逻辑结合现实找到隐藏的最佳可能性）',
-    'G': '（话题走向过于愚蠢或荒谬，根据话题内容用怀疑对方智商的语气一针见血30字以内地搞笑回应）',
-    'H': '（可用两三句有趣的方式简单30字以内回应。坚决不要解释在开玩笑以及为什么这样说）',
-    'I': '（这是在开玩笑，请用两三句搞笑、调侃的方式简单30字以内回应。坚决不要解释在开玩笑以及为什么这样说）',
+    'G': '（话题走向过于简单无意义，可能在测试你的能力，请进行30字以内回应）',
+    'H': '',
+    'I': '（请用两三句简单30字以内回应）',
     'J': '',
     'K': '',
-    'L': '（话题走向不理智，请先表示理解情绪，然后进行安抚，慰问更多细节）',
+    'L': '（先正常回应，然后询问更多细节）',
     'M': '',
-    'N': '（可用两三句有趣的方式简单30字以内回应，坚决不要解释在开玩笑以及为什么这样说）',
+    'N': '（可用两三句简单30字以内回应）',
     'O': '',
 }
 
@@ -144,7 +158,10 @@ def compile_message(message: Message) -> str:
     classify_result = message.remark.get("classify", "")
     if len(classify_result) == 0:
         return message.remark["raw"]
-    classify_letter = classify_result[0].upper()
+    match = re.search(r'Classification:\s*(\w)', classify_result)
+    if match is None:
+        return message.remark["raw"]
+    classify_letter = match.group(1)
     tip = classify_tip_map.get(classify_letter, '')
     if len(tip) == 0:
         return message.remark["raw"]
